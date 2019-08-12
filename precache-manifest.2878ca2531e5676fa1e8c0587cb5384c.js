99c668365d1749bfdd3d58ae3fba0d4abdcaabd3
rrankit <rankit.ranjan.contractor@move.com>
2019-08-13 00:58:58 +0530

    Updates

---
+++ precache-manifest.2878ca2531e5676fa1e8c0587cb5384c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99863ce9d641e1d340360b1684e39fd7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d9323b57775019f2a8b1",
+    "url": "/monsters-rolodex/static/css/main.d72b079f.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monsters-rolodex/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "d9323b57775019f2a8b1",
+    "url": "/monsters-rolodex/static/js/main.4e169832.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
